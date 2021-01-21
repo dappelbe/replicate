f82066e1b152d7b8f76a70d463bee7f09dd6108b
Duncan Appelbe <duncan.appelbe@ndorms.ox.ac.uk>
2021-01-21 20:41:24 +0000

    Removed files as regenerating the library

--- src/NDORMS/REDCap/fred.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../../../vendor/autoload.php';
-
-$lexer = new \NDORMS\REDCap\REDCapLexer();
-$input = "[fred] = [frank]";
-$actual = $lexer->tokenize( $lexer->stripTabsAndBreaks($input));
-$aa = "";
